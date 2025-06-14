363e64496f384d8c5bca45f93c2d93c9c8b3dd9e
melixyz1618 <melixyz@gmail.com>
2025-06-14 13:16:13 +0300

    Güneş ve Ay eklendi

---
+++ flask_api.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify
+from datetime import datetime
+from skyfield.api import load_file, load
+
+app = Flask(__name__)
+
+# Efemeris verilerini yükle
+planets = load_file('de421.bsp')
+ts = load.timescale()
+earth = planets['earth']
+
+# Güneş ve Ay dahil tüm gök cisimleri
+planet_names = [
+    ('Mercury', 'Merkür'),
+    ('Venus', 'Venüs'),
+    ('Mars', 'Mars'),
+    ('Jupiter Barycenter', 'Jüpiter'),
+    ('Saturn Barycenter', 'Satürn'),
+    ('Uranus Barycenter', 'Uranüs'),
+    ('Neptune Barycenter', 'Neptün'),
+    ('Sun', 'Güneş'),
+    ('Moon', 'Ay')
+]
+
+@app.route('/mesafeler')
+def get_distances():
+    now = datetime.utcnow()
+    t = ts.utc(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    data = []
+
+    for eng_name, turkish_name in planet_names:
+        planet = planets[eng_name]
+        distance_au = earth.at(t).observe(planet).apparent().distance().au
+        distance_km = distance_au * 149_597_870.7
+        data.append({
+            'gezegen': turkish_name,
+            'mesafe_km': round(distance_km)
+        })
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
